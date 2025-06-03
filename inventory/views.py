6904717167a1d893a48629d1f4f1c58bf77e9508
Abhay Braja <72004597+abhaybraja@users.noreply.github.com>
2025-06-03 23:42:14 +0530

    cache_page for 2 hours

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from inventory.serializers import ProductSerializer
 from .models import Product
@@ -9,6 +11,7 @@ from .models import Product
 class ProductAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request):
         products = Product.objects.all()
         data = ProductSerializer(products,many=True).data
