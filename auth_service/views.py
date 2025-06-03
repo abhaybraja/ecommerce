0e3b2d3fd5f1823d16c756a660e2378e99e13aed
Abhay Braja <72004597+abhaybraja@users.noreply.github.com>
2025-06-03 23:48:01 +0530

    Profile api added

--- auth_service/views.py
+++ auth_service/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from auth_service.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
+class ProfileView(APIView):
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_headers("Authorization"))
+    def get(self, request, format=None):
+        profile = UserSerializer(request.user).data
+        return Response(profile)
